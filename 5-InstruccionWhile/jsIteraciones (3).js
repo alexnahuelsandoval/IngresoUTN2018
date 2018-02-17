a7a000c5f2b8f7c31e6249bc392884baee131a1a
alexnahuelsandoval <36246849+alexnahuelsandoval@users.noreply.github.com>
2018-02-16 22:14:51 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -2,6 +2,8 @@ function Mostrar()
 {
 
 var clave = prompt("ingrese el número clave.");
-
-
+while (clave != "utn750"){
+    clave = prompt(" reingrese la clave");
+}
+alert("clave correcta");
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -2,6 +2,15 @@ function Mostrar()
 {
 
 	var numero = prompt("ingrese un número entre 0 y 10.");
+	
+	while (!(numero  >=1 && numero <=10)){
+		numero= prompt("dato incorrecto");
+	}
+	document.getElementById("Numero").value = numero;
+	
+	
+
+
 
 
 }//FIN DE LA FUNCIÓN
